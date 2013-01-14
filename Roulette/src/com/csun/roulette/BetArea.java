9be4d38023e2be1d0a3d5028fff07fb890e93639
Chan Nguyen <atbl1511@gmail.com>
2013-01-13 23:34:35 -0800

    Finish up drawing betting area.

--- Roulette/src/com/csun/roulette/BetArea.java
+++ Roulette/src/com/csun/roulette/BetArea.java
@@ -1,10 +1,22 @@
 package com.csun.roulette;
 
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import junit.framework.Assert;
+
 import android.graphics.Canvas;
 import android.graphics.Paint;
 
 public class BetArea implements Drawable {
 	private static final Paint paint;
+	private static Map<String, BetArea> betAreaHashMap;
+
+	private interface BettingFunctor {
+		public boolean isIt(BetArea betArea);
+	}
+	
 	static {
 		 paint = new Paint(Paint.ANTI_ALIAS_FLAG);
 		 paint.setStyle(Paint.Style.FILL);
@@ -18,6 +30,21 @@ public class BetArea implements Drawable {
 		THREE_NUMBER,
 		FOUR_NUMBER,
 		SIX_NUMBER,
+		
+		TWO_TO_ONE,
+		ONE_TO_TWELVE,
+		LARGE_18_NUMBER,
+		SMALL_18_NUMBER,
+		ODD,
+		EVEN,
+		RED,
+		BLACK
+	}
+	
+	public enum NumberColor {
+		RED,
+		BLACK,
+		SPECIAL,
 	}
 
 	private final int x;
@@ -25,7 +52,12 @@ public class BetArea implements Drawable {
 	private final int width;
 	private final int height;
 	private final BetType betType;
-
+	private String number;
+	private NumberColor color;
+	
+	public static void setLookupTable(Map<String, BetArea> map) {
+		betAreaHashMap = map;
+	}
 
 	public BetArea(int x, int y, BetType betType, int width, int height) {
 		this.x = x;
@@ -33,6 +65,24 @@ public class BetArea implements Drawable {
 		this.betType = betType;
 		this.width = width;
 		this.height = height;
+		this.number = "99";
+		this.color = NumberColor.SPECIAL;
+	}
+	
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	
+	public void setNumberColor(NumberColor color) {
+		this.color = color;
+	}
+	
+	public String getNumber() {
+		return number;
+	}
+	
+	public NumberColor getNumberColor() {
+		return color;
 	}
 	
 	public int getX() {
@@ -73,6 +123,73 @@ public class BetArea implements Drawable {
 			case SIX_NUMBER:
 				canvas.drawRect(x - width, y - 3*height, x + width, y, paint);
 				break;
+				
+			case TWO_TO_ONE:
+				canvas.drawRect(x - (12*width + width/2), y - height/2, x - width/2, y + height/2, paint);
+				break;
+				
+			case ONE_TO_TWELVE:
+				canvas.drawRect(x - 2*width, y - 3*height - height/2, x + 2*width, y - height/2, paint);
+				break;
+				
+			case SMALL_18_NUMBER:
+				canvas.drawRect(x - width, y - 4*height - height/2, x + 5*width, y - height/2 - height, paint);
+				break;
+				
+			case LARGE_18_NUMBER:
+				canvas.drawRect(x - 5*width, y - 4*height - height/2, x + width, y - height/2 - height, paint);
+				break;
+				
+			case ODD:
+				drawIf(canvas, new BettingFunctor() {
+					@Override
+					public boolean isIt(BetArea betArea) {
+						int n = Integer.parseInt(betArea.getNumber());
+						return (n != 99 && n % 2 == 1);
+					}
+				});
+				break;
+				
+			case EVEN:
+				drawIf(canvas, new BettingFunctor() {
+					@Override
+					public boolean isIt(BetArea betArea) {
+						int n = Integer.parseInt(betArea.getNumber());
+						return (n != 99 && n % 2 == 0);
+					}
+				});
+				break;
+				
+			case RED:
+				drawIf(canvas, new BettingFunctor() {
+					@Override
+					public boolean isIt(BetArea betArea) {
+						return betArea.getNumberColor() == NumberColor.RED;
+					}
+				});
+				break;
+				
+			case BLACK:
+				drawIf(canvas, new BettingFunctor() {
+					@Override
+					public boolean isIt(BetArea betArea) {
+						return betArea.getNumberColor() == NumberColor.BLACK;
+					}
+				});
+				break;
+		}
+	}
+	
+	private void drawIf(Canvas canvas, BettingFunctor predicate) {
+		int x = 0;
+		int y = 0;
+		for (Entry<String, BetArea> entry : betAreaHashMap.entrySet()) {
+	        if (predicate.isIt(entry.getValue())) {
+		        String position = entry.getKey();
+		        x = Integer.parseInt(position.substring(0, position.indexOf(":")));
+		        y = Integer.parseInt(position.substring(position.indexOf(":") + 1));
+		        canvas.drawRect(x - width/2, y - height/2, x + width/2, y + height/2, paint);
+	        }
 		}
 	}
 }
--- Roulette/src/com/csun/roulette/RouletteTable.java
+++ Roulette/src/com/csun/roulette/RouletteTable.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Map;
 
 import com.csun.roulette.BetArea.BetType;
+import com.csun.roulette.BetArea.NumberColor;
 
 import android.content.Context;
 import android.graphics.Bitmap;
@@ -29,13 +30,13 @@ public class RouletteTable extends View {
 	 * Constants
 	 */
 	private static final int DISTANCE_FROM_FINGER = 100;
-	private static final int TEXT_SIZE = 20;
+	private static final int TEXT_SIZE = 15;
 	
-	private static final int OFFSET_X = 40;
-	private static final int OFFSET_Y = 60;
+	private static final int OFFSET_X = 60;
+	private static final int OFFSET_Y = 80;
 	
-	private static final int BET_SQUARE_WIDTH = 40;
-	private static final int BET_SQUARE_HEIGHT = 60;
+	private static final int BET_SQUARE_WIDTH = 60;
+	private static final int BET_SQUARE_HEIGHT = 80;
 	
 	private static final int TABLE_ROWS = 3;
 	private static final int TABLE_COLUMNS = 12;
@@ -47,15 +48,39 @@ public class RouletteTable extends View {
 	private static final int LOWER_BOUNDARY_Y = OFFSET_Y;
 	private static final int UPPER_BOUNDARY_Y = OFFSET_Y + (BET_SQUARE_HEIGHT * TABLE_ROWS);
 	
+	private static final NumberColor[] BET_COLORS = {
+		//    1					2                  3
+		NumberColor.RED,   NumberColor.BLACK, NumberColor.RED,
+		//    4					5                  6
+		NumberColor.BLACK, NumberColor.RED,   NumberColor.BLACK,
+		//    7					8                  9
+		NumberColor.RED,   NumberColor.BLACK, NumberColor.RED,
+		//    10				11                 12
+		NumberColor.BLACK, NumberColor.BLACK, NumberColor.RED,
+		//    13				14                 15
+		NumberColor.BLACK, NumberColor.RED,   NumberColor.BLACK,
+		//    16				17                 18
+		NumberColor.RED,   NumberColor.BLACK, NumberColor.RED,
+		//    19				20                 21 
+		NumberColor.RED,   NumberColor.BLACK, NumberColor.RED,
+		//    22				23                 24 
+		NumberColor.BLACK, NumberColor.RED,   NumberColor.BLACK,
+		//    25				26                 27 
+		NumberColor.RED,   NumberColor.BLACK, NumberColor.RED,
+		//    28				29                 30 
+		NumberColor.BLACK, NumberColor.BLACK, NumberColor.RED,
+		//    31				32                 33 
+		NumberColor.BLACK, NumberColor.RED,   NumberColor.BLACK,
+		//    34				35                 36 
+		NumberColor.RED,   NumberColor.BLACK, NumberColor.RED
+	};
+	
 	private Paint tablePaint;
 	private Paint textPaint;
 	private Paint betPaint;
 
-	
 	private List<Chip> chips;
-	
 	private RouletteWheel wheel;
-	
 	private boolean inDrawArea;
 	private Map<String, BetArea> betAreas;
 	
@@ -82,6 +107,7 @@ public class RouletteTable extends View {
 	private void initialize(Context context) {
 		initializeUi();
 	    initializeCentersBetSquare();
+	    BetArea.setLookupTable(betAreas);
 	    
 	    // others
 	    wheel = new RouletteWheel(120, 120, 100);
@@ -116,10 +142,15 @@ public class RouletteTable extends View {
 		String key = "";
 		boolean alternate = true;
 		// centers
+		int counter = 1;
 		for (int x = LOWER_BOUNDARY_X + halfWidth; x < UPPER_BOUNDARY_X; x += BET_SQUARE_WIDTH) {
-			for (int y = LOWER_BOUNDARY_Y + halfHeight; y < UPPER_BOUNDARY_Y + halfHeight; y += BET_SQUARE_HEIGHT) {
+			for (int y = UPPER_BOUNDARY_Y - halfHeight; y > LOWER_BOUNDARY_Y; y -= BET_SQUARE_HEIGHT) {
 				key = x + ":" + y;
-				betAreas.put(key, new BetArea(x, y, BetArea.BetType.SINGLE_NUMBER, BET_SQUARE_WIDTH, BET_SQUARE_HEIGHT));
+				BetArea ba = new BetArea(x, y, BetArea.BetType.SINGLE_NUMBER, BET_SQUARE_WIDTH, BET_SQUARE_HEIGHT);
+				ba.setNumber(Integer.toString(counter));
+				ba.setNumberColor(BET_COLORS[counter - 1]);
+				betAreas.put(key, ba);
+				counter++;
 			}
 		}
 		
@@ -164,30 +195,192 @@ public class RouletteTable extends View {
 				alternate = true;
 			}
 		}
+		
+		int x = UPPER_BOUNDARY_X + halfWidth;
+		for (y = LOWER_BOUNDARY_Y + halfHeight; y < UPPER_BOUNDARY_Y; y += BET_SQUARE_HEIGHT) {
+			key = x + ":" + y;
+			betAreas.put(key, new BetArea(x, y, BetArea.BetType.TWO_TO_ONE, BET_SQUARE_WIDTH, BET_SQUARE_HEIGHT));
+		}
+		
+		y = UPPER_BOUNDARY_Y + halfHeight;
+		for (x = LOWER_BOUNDARY_X + 2*BET_SQUARE_WIDTH; x < UPPER_BOUNDARY_X; x += 4*BET_SQUARE_WIDTH) {
+			key = x + ":" + y;
+			betAreas.put(key, new BetArea(x, y, BetArea.BetType.ONE_TO_TWELVE, BET_SQUARE_WIDTH, BET_SQUARE_HEIGHT));
+		}
+		
+		x = LOWER_BOUNDARY_X + BET_SQUARE_WIDTH;
+		y = UPPER_BOUNDARY_Y + BET_SQUARE_HEIGHT + halfHeight;
+		key = x + ":" + y;
+		betAreas.put(key, new BetArea(x, y, BetArea.BetType.SMALL_18_NUMBER, BET_SQUARE_WIDTH, BET_SQUARE_HEIGHT));
+		
+		x = UPPER_BOUNDARY_X - BET_SQUARE_WIDTH;
+		y = UPPER_BOUNDARY_Y + BET_SQUARE_HEIGHT + halfHeight;
+		key = x + ":" + y;
+		betAreas.put(key, new BetArea(x, y, BetArea.BetType.LARGE_18_NUMBER, BET_SQUARE_WIDTH, BET_SQUARE_HEIGHT));
+		
+		x = LOWER_BOUNDARY_X + 3*BET_SQUARE_WIDTH;
+		y = UPPER_BOUNDARY_Y + BET_SQUARE_HEIGHT + halfHeight;
+		key = x + ":" + y;
+		betAreas.put(key, new BetArea(x, y, BetArea.BetType.EVEN, BET_SQUARE_WIDTH, BET_SQUARE_HEIGHT));
+		
+		x = UPPER_BOUNDARY_X - 3*BET_SQUARE_WIDTH;
+		y = UPPER_BOUNDARY_Y + BET_SQUARE_HEIGHT + halfHeight;
+		key = x + ":" + y;
+		betAreas.put(key, new BetArea(x, y, BetArea.BetType.ODD, BET_SQUARE_WIDTH, BET_SQUARE_HEIGHT));
+		
+		x = LOWER_BOUNDARY_X + 5*BET_SQUARE_WIDTH;
+		y = UPPER_BOUNDARY_Y + BET_SQUARE_HEIGHT + halfHeight;
+		key = x + ":" + y;
+		betAreas.put(key, new BetArea(x, y, BetArea.BetType.RED, BET_SQUARE_WIDTH, BET_SQUARE_HEIGHT));
+				
+		x = UPPER_BOUNDARY_X - 5*BET_SQUARE_WIDTH;
+		y = UPPER_BOUNDARY_Y + BET_SQUARE_HEIGHT + halfHeight;
+		key = x + ":" + y;
+		betAreas.put(key, new BetArea(x, y, BetArea.BetType.BLACK, BET_SQUARE_WIDTH, BET_SQUARE_HEIGHT));
+		
+		x = LOWER_BOUNDARY_X - 5*BET_SQUARE_WIDTH;
+		
 	}
 	
 	private void drawTable(Canvas canvas) {
-		betPaint.setColor(Color.RED);
-		for (int x = LOWER_BOUNDARY_X; x < UPPER_BOUNDARY_X; x += BET_SQUARE_WIDTH) {
-			for (int y = LOWER_BOUNDARY_Y; y < UPPER_BOUNDARY_Y; y += BET_SQUARE_HEIGHT) {
-				canvas.drawRect(x, y, x + BET_SQUARE_WIDTH, y + BET_SQUARE_HEIGHT, betPaint);
-				if (betPaint.getColor() == Color.RED) {
+		int halfWidth = BET_SQUARE_WIDTH/2;
+		int halfHeight = BET_SQUARE_HEIGHT/2;
+		int counter = 0;
+		for (int x = LOWER_BOUNDARY_X + halfWidth; x < UPPER_BOUNDARY_X; x += BET_SQUARE_WIDTH) {
+			for (int y = UPPER_BOUNDARY_Y - halfHeight; y > LOWER_BOUNDARY_Y; y -= BET_SQUARE_HEIGHT) {
+				if (BET_COLORS[counter] == NumberColor.BLACK) {
 					betPaint.setColor(Color.BLACK);
-				} else {
+				} else if (BET_COLORS[counter] == NumberColor.RED) {
 					betPaint.setColor(Color.RED);
+				} else {
+					betPaint.setColor(Color.GREEN);
 				}
+				canvas.drawRect(x - halfWidth, y - halfHeight, x + halfWidth, y + halfHeight, betPaint);
+				// draw border
+				betPaint.setStyle(Style.STROKE);
+				betPaint.setColor(Color.WHITE);
+				canvas.drawRect(x - halfWidth, y - halfHeight, x + halfWidth, y + halfHeight, betPaint);
+				
+				// draw tex
+				betPaint.setStyle(Style.FILL);
+				betPaint.setColor(Color.GREEN);
+				canvas.drawText(Integer.toString(counter + 1), x, y, betPaint);
+				counter++;
 			}
 		}
+		
+		// draw left panel
+		betPaint.setStyle(Style.FILL);
+		betPaint.setColor(Color.GREEN);
+		canvas.drawRect(
+				LOWER_BOUNDARY_X - BET_SQUARE_WIDTH, 
+				LOWER_BOUNDARY_Y,
+				LOWER_BOUNDARY_X,
+				UPPER_BOUNDARY_Y, betPaint);
+		
+		betPaint.setStyle(Style.STROKE);
+		betPaint.setColor(Color.WHITE);
+		canvas.drawRect(
+				LOWER_BOUNDARY_X - BET_SQUARE_WIDTH, 
+				LOWER_BOUNDARY_Y,
+				LOWER_BOUNDARY_X,
+				UPPER_BOUNDARY_Y, betPaint);
+		
+		betPaint.setStyle(Style.FILL);
+		betPaint.setColor(Color.BLACK);
+		betPaint.setTextSize(TEXT_SIZE);
+		canvas.drawText("0", 
+			LOWER_BOUNDARY_X - BET_SQUARE_WIDTH + BET_SQUARE_WIDTH / 2, 
+			LOWER_BOUNDARY_Y + 3 * BET_SQUARE_HEIGHT / 2,
+			betPaint);
+			
+		
+		// draw right panel
+		for (int y = LOWER_BOUNDARY_Y; y < UPPER_BOUNDARY_Y; y += BET_SQUARE_HEIGHT) {
+			betPaint.setStyle(Style.FILL);
+			betPaint.setColor(Color.GREEN);
+			canvas.drawRect(
+				UPPER_BOUNDARY_X,
+				y,
+				UPPER_BOUNDARY_X + BET_SQUARE_WIDTH,
+				y + BET_SQUARE_HEIGHT, betPaint);
+			
+			betPaint.setStyle(Style.STROKE);
+			betPaint.setColor(Color.WHITE);
+			canvas.drawRect(
+				UPPER_BOUNDARY_X,
+				y,
+				UPPER_BOUNDARY_X + BET_SQUARE_WIDTH,
+				y + BET_SQUARE_HEIGHT, betPaint);
+			
+			betPaint.setStyle(Style.FILL);
+			betPaint.setColor(Color.BLACK);
+			betPaint.setTextSize(TEXT_SIZE);
+			canvas.drawText("2:1", UPPER_BOUNDARY_X + BET_SQUARE_WIDTH/3, y + BET_SQUARE_HEIGHT/2, betPaint);
+		}
+		
+		// draw bottom panel
+		String[] section = {"1st", "2nd", "3rd"};
+		int i = 0;
+		for (int x = LOWER_BOUNDARY_X; x < UPPER_BOUNDARY_X; x += 4*BET_SQUARE_WIDTH) {
+			drawSpecialBetArea(canvas, x, UPPER_BOUNDARY_Y, 4*BET_SQUARE_WIDTH, BET_SQUARE_HEIGHT, betPaint, section[i] + " 12");
+			i++;
+		}
+		
+		int x = LOWER_BOUNDARY_X;
+		int y = UPPER_BOUNDARY_Y + BET_SQUARE_HEIGHT;
+		drawSpecialBetArea(canvas, x, y, 2*BET_SQUARE_WIDTH, BET_SQUARE_HEIGHT, betPaint, "1-18");
+		x += 2*BET_SQUARE_WIDTH;
+		drawSpecialBetArea(canvas, x, y, 2*BET_SQUARE_WIDTH, BET_SQUARE_HEIGHT, betPaint, "EVEN");
+		x += 2*BET_SQUARE_WIDTH;
+		drawSpecialBetArea(canvas, x, y, 2*BET_SQUARE_WIDTH, BET_SQUARE_HEIGHT, betPaint, "RED");
+		x += 2*BET_SQUARE_WIDTH;
+		drawSpecialBetArea(canvas, x, y, 2*BET_SQUARE_WIDTH, BET_SQUARE_HEIGHT, betPaint, "BLACK");
+		x += 2*BET_SQUARE_WIDTH;
+		drawSpecialBetArea(canvas, x, y, 2*BET_SQUARE_WIDTH, BET_SQUARE_HEIGHT, betPaint, "ODD");
+		x += 2*BET_SQUARE_WIDTH;
+		drawSpecialBetArea(canvas, x, y, 2*BET_SQUARE_WIDTH, BET_SQUARE_HEIGHT, betPaint, "19-36");
+	}
+	
+	private void drawSpecialBetArea(Canvas canvas, int x, int y, int width, int height, Paint paint, String text) {
+		paint.setStyle(Style.FILL);
+		paint.setColor(Color.GREEN);
+		canvas.drawRect(x, y, x + width, y + height, paint);
+		paint.setStyle(Style.STROKE);
+		paint.setColor(Color.WHITE);
+		canvas.drawRect(x, y, x + width, y + height, paint);
+		paint.setStyle(Style.FILL);
+		paint.setColor(Color.BLACK);
+		paint.setTextSize(TEXT_SIZE);
+		canvas.drawText(text, x + width/3, y + height/2, paint);
+	}
+	
+	public void drawNumbers(Canvas canvas) {
+		canvas.save();
+		betPaint.setColor(Color.GREEN);
+		betPaint.setTextSize(TEXT_SIZE);
+		int halfWidth = BET_SQUARE_WIDTH / 2;
+		int halfHeight = BET_SQUARE_HEIGHT / 2;
+		int counter = 1;
+		for (int x = LOWER_BOUNDARY_X; x < UPPER_BOUNDARY_X; x += BET_SQUARE_WIDTH) {
+			for (int y = UPPER_BOUNDARY_Y; y > LOWER_BOUNDARY_Y; y -= BET_SQUARE_HEIGHT) {
+				canvas.drawText(Integer.toString(counter), x + halfWidth , y - halfHeight, betPaint);
+				counter++;
+			}
+		}
+		canvas.restore();
 	}
 	
 	@Override
 	protected void onDraw(Canvas canvas) {
 		super.onDraw(canvas);
 		drawTable(canvas);
+		// drawNumbers(canvas);
 		// wheel.draw(canvas);
 		for (Chip c : chips) {
 			c.draw(canvas);
 		}
+		
 		currentChip.draw(canvas);
 		if (inDrawArea) {
 			drawBetRectangle(canvas, (int)currentFingerX, (int)currentFingerY);
